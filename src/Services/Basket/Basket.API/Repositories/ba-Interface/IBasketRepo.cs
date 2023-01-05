c59908595d50f115f9c0eac394214d6055da9997
sogunsanya <109689290+sogunsanya@users.noreply.github.com>
2023-01-05 00:52:22 +0000

    Basket.API Implementation started

---
+++ src/Services/Basket/Basket.API/Repositories/ba-Interface/IBasketRepo.cs
@@ -0,0 +1,16 @@
+﻿using Basket.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories.ba_Interface
+{
+    public interface IBasketRepo
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart shoppedBasket);
+        Task DeleteBasket(string userName);
+
+    }
+}
